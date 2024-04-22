3e9d9fa38286f85e81328ce99e12d1067238dd5a
João Amestoy <167258406+JoaoVictorSouzaAmestoy@users.noreply.github.com>
2024-04-22 14:55:11 -0300

    Adição de troca de tela após cálculo de imc

--- app/src/main/java/br/com/teste/treinoapp/IMCMain.java
+++ app/src/main/java/br/com/teste/treinoapp/IMCMain.java
@@ -21,10 +21,32 @@ public class IMCMain extends AppCompatActivity {
         tResultado = findViewById(R.id.tResultado);
     }
     public void somas (View view){
+        try{
         float valor1 = Float.parseFloat(etPeso.getText().toString());
         float valor2 = Float.parseFloat(etAltura.getText().toString());
         float Resultado = valor1/(valor2*valor2);
-        String resultadoCalculoFinal = String.valueOf(Resultado);
+        String resultadoCalculoFinal = String.valueOf(imc);
         tResultado.setText(resultadoCalculoFinal);
+
+        navegarParaTelaApropriada(imc);
+    } catch (NumberFormatException e) {
+        tResultado.setText("Por favor, insira valores válidos.");
+        }
+    }
+
+    private void navegarParaTelaApropriada(float imc) {
+        if (imc < 18.5) {
+            Intent intent = new Intent(this, AbaixoPesoMain.class);
+            startActivity(intent);
+        } else if (imc >= 18.5 && imc < 25) {
+            startActivity(new Intent(this, PesoNormalMain.class));
+        } else if (imc >= 25 && imc < 30) {
+            startActivity(new Intent(this, ExcessoPesoMain.class));
+        } else if (imc >= 30 && imc < 40) {
+            startActivity(new Intent(this, ObesidadePesoMain.class));
+        } else if (imc >= 40) {
+            startActivity(new Intent(this, ObesidadeExtremaPesoMain.class));
+        }
     }
 }
+    
